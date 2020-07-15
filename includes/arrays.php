69beda757e4ea808736a7ff5414bb73063db0ead
Drew Mercer <japanexpress@gmail.com>
2020-07-15 12:56:11 -0400

    menu is populating dynamically

--- includes/arrays.php
+++ includes/arrays.php
@@ -1,5 +1,4 @@
 <?php 
-// Nav menu items
 $navItems = array(
 
     array(
@@ -18,7 +17,6 @@ $navItems = array(
         slug => 'contact.php',
         title => 'Contact'
     )
-
     );
 
 $teamMembers = array(
@@ -39,7 +37,45 @@ $teamMembers = array(
         position => 'Cashier',
         bio => 'Tina looks for ways to consolidate service and increase table turns. She applies a positive suggestive sales approach to guide Guests in trying new menu items.',
         img => 'tina'
+    )
+);
+
+$menuItems = array(
+    'southern-sandwich' => array(
+        title => 'Southern Sandwich',
+        price => 11,
+        blurb => 'roasted turkey, balsamic tomato, white cheddar, black pepper back, mornay sauce',
+        drink => 'Sweet Tea'
     ),
-
-
+    'cubano-sandwich' => array(
+        title => 'Cubano Sandwich',
+        price => 11,
+        blurb => 'pork shoulder, ham, swiss, dijon, bread-n-butter pickle, mojo sauce',
+        drink => 'Café Cubano'
+    ),
+    'frenchy-sandwich' => array(
+        title => 'Frenchy Sandwich',
+        price => 12,
+        blurb => 'garlic and herb roasted beef, smoked gouda, caramelized onion, horsey sauce, au jus',
+        drink => 'Citron Presse'
+    ),
+    'abcdglt-sandwich' => array(
+        title => 'ABCDGLT Sandwich',
+        price => 10,
+        blurb => 'avocado, bacon, chow chow, dilly goat, lettuce, tomato',
+        drink => 'Cheerwine'
+    ),
+    'russian-sandwich' => array(
+        title => 'Russian Sandwich',
+        price => 10,
+        blurb => 'house made pastrami, swiss, coleslaw, russian dressing',
+        drink => 'Sparkling Water'
+    ),
+    'potato-salad' => array(
+        title => 'Raleigh\'s Best Potato Salad (16oz)',
+        price => 7,
+        blurb => 'the best potato salad you have ever had',
+        // drink => 'Sweet Tea'
+    ),
+    
 );
--- menu.php
+++ menu.php
@@ -1,6 +1,24 @@
-<?php 
+<?php
 define("TITLE", "Menu | Linus & Pepper's Raleigh");
 include('includes/header.php');
 ?>
+
+<div id="menu-items">
+    <h1>Our Delicious Menu</h1>
+    <p>all sammies come with choice of 1 side, but you should choose the potato salad
+    </p>
+    <p><em>Click any menu item to learn more about it.</em></p>
+    <hr>
+
+    <ul>
+        <?php foreach ($menuItems as $dish => $item) { ?>
+
+            <li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item[title]; ?> </a><sup>$</sup> <?php echo $item[price]; ?> </li>
+
+        <?php } ?>
+
+    </ul>
+</div>
+
+<hr>
 <?php include('includes/footer.php');
-?>
